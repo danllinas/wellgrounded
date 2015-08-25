030b3c1996bb31af6d68acaf952442ee932ccc24
Daniel Llinas <danllinas@gmail.com>
2015-08-25 19:34:40 -0400

    Organizing exercises into chapters.

---
+++ chapter1/c2fin.rb
@@ -0,0 +1,5 @@
+puts "Reading Celsius temp from data file..."
+num = File.read("temp.dat")
+celsius = num.to_i
+farenheit = (celsius * 9/5) + 32
+puts "The number is #{num} and the result is #{farenheit}"
