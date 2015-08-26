aac5c05357f9885f52b9b7921636cfe7c1d29161
Daniel Llinas <danllinas@gmail.com>
2015-08-25 22:35:27 -0400

    Starting chapter 2. Deleted a bunch of stuff that I don't know how it ended up there.

--- chapter1/doc/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var search_data = {"index":{"searchIndex":["temp.dat","temp.out"],"longSearchIndex":["",""],"info":[["temp.dat","","temp_dat.html","","<p>25\n"],["temp.out","","temp_out.html","","<p>77\n"]]}}
